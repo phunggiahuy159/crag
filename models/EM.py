1d1e00376887d887525f99967f1403678e541977
phunggiahuy159 <159228989+phunggiahuy159@users.noreply.github.com>
2025-06-12 16:00:05 +0700

    Update EM.py

--- models/EM.py
+++ models/EM.py
@@ -2,4 +2,4 @@ from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
 import streamlit as st
 
 embedding = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004", google_api_key='AIzaSyCyXr2KjwW58Vm0bewJ_sGEau8C1WS_QNQ'
+    model="models/text-embedding-004", google_api_key='AIzaSyCyXr2KjwW58Vm0bewJ_sGEau8C1WS_QNQ')
