5315654ac08f3fb7dabd34911799b8a7e4048891
phunggiahuy159 <159228989+phunggiahuy159@users.noreply.github.com>
2025-06-18 22:43:22 +0700

    Update EM to bge m3

--- models/EM.py
+++ models/EM.py
@@ -1,5 +1,11 @@
-from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
-import streamlit as st
+# from langchain_google_genai.embeddings import GoogleGenerativeAIEmbeddings
+# import streamlit as st
 
-embedding = GoogleGenerativeAIEmbeddings(
-    model="models/text-embedding-004", google_api_key='AIzaSyCyXr2KjwW58Vm0bewJ_sGEau8C1WS_QNQ')
+# embedding = GoogleGenerativeAIEmbeddings(
+#     model="models/text-embedding-004", google_api_key='AIzaSyCyXr2KjwW58Vm0bewJ_sGEau8C1WS_QNQ')
+from langchain.embeddings import HuggingFaceEmbeddings
+
+# Load an offline embedding model
+embedding = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-small-en-v1.5"
+)
