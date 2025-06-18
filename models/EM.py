c81f47ba86f04c098da07545064a7113bd1731c7
phunggiahuy159 <159228989+phunggiahuy159@users.noreply.github.com>
2025-06-18 23:53:36 +0700

    Update EM.py

--- models/EM.py
+++ models/EM.py
@@ -3,9 +3,37 @@
 
 # embedding = GoogleGenerativeAIEmbeddings(
 #     model="models/text-embedding-004", google_api_key='AIzaSyCyXr2KjwW58Vm0bewJ_sGEau8C1WS_QNQ')
-from langchain.embeddings import HuggingFaceEmbeddings
+# from langchain.embeddings import HuggingFaceEmbeddings
 
-# Load an offline embedding model
-embedding = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5"
-)
+# # Load an offline embedding model
+# embedding = HuggingFaceEmbeddings(
+#     model_name="BAAI/bge-small-en-v1.5"
+# )
+from langchain.embeddings.base import Embeddings
+from sentence_transformers import SentenceTransformer
+import torch
+
+class VietnameseSBERTEmbeddings(Embeddings):
+    def __init__(self, model_name="AITeamVN/Vietnamese_Embedding", device=None):
+        self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = SentenceTransformer(model_name)
+        self.model.to(self.device)
+
+    def embed_documents(self, texts):
+        return self.model.encode(
+            texts,
+            convert_to_numpy=True,
+            batch_size=128,
+            show_progress_bar=True,
+            device=self.device
+        )
+
+    def embed_query(self, text):
+        return self.model.encode(
+            [text],
+            convert_to_numpy=True,
+            batch_size=1,
+            show_progress_bar=False,
+            device=self.device
+        )[0]
+embedding = VietnameseSBERTEmbeddings()
